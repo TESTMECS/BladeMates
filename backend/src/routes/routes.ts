b61eb21dbf8fe93b58f451fef4a527fb52a251df
Zain B <85662816+SpicyMcSpice1938@users.noreply.github.com>
2024-12-07 13:58:43 -0500

    Revert "search by tag route"

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -4,7 +4,6 @@ import { authRouter } from './auth';
 import { articleRouter } from './article';
 import { userRouter } from './user';
 import { friendRouter } from './friend';
-import { searchRouter } from './globalSearch';
 import { globalArticlesRouter } from './globalArticles';
 
 export function createRoutesWith(app: Express) {
@@ -17,8 +16,6 @@ export function createRoutesWith(app: Express) {
 
   app.use('/api/user', userRouter);
   app.use('/api/global', globalArticlesRouter);
-
-  app.use('/api/search', searchRouter);
 
   app.use('*', (_, res) => {
     res.status(404).json({ error: 'Not found' });
