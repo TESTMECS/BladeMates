fe9118f7e6f61926d5ae3cac4bbd770e7e34c011
Andrew Warkentin <axwar7410@gmail.com>
2024-12-11 21:22:13 -0500

    addtional commits

--- backend/src/data/article.ts
+++ backend/src/data/article.ts
@@ -1,12 +1,9 @@
 import { users } from '../config/mongoCollections';
 import { validateWithType, StatusError, validate } from '../utils/Error';
-
 import { ObjectId, PushOperator } from 'mongodb';
 import { userSchema } from '../validation/user';
-
 import { User } from '../types/mongo';
-import { sendNotification } from '../services/rabbitmqProducer';
-
+// import { sendNotification } from '../services/rabbitmqProducer';
 /**
  * A function that adds a notification to all users who have followed the user with id `userId`
  * @param userId
@@ -14,26 +11,24 @@ import { sendNotification } from '../services/rabbitmqProducer';
  */
 const addNotifications = async (userId: string, articleId: string) => {
   // sendNotification(userId);
-
   const usersCollection = await users();
-
   const followers = await usersCollection
     .find({
       friends: { $elemMatch: { _id: new ObjectId(userId) } },
     })
     .toArray();
-
+  console.log("userId", userId);
+  console.log("articleId", articleId);
   const notification = {
-    _id: ObjectId.createFromTime(Date.now() / 1000),
+    _id: new ObjectId(),
     friendId: new ObjectId(userId),
-    articleId: new ObjectId(articleId),
+    articleId: articleId,
     read: false,
   };
-
   const followerIds = followers.map((follower) => follower._id);
-
-  console.log(followerIds);
-
+  if (followerIds.length === 0) {
+    return;
+  }
   const insertedInfo = await usersCollection.updateMany(
     {
       _id: { $in: followerIds },
@@ -44,44 +39,34 @@ const addNotifications = async (userId: string, articleId: string) => {
       } as unknown as PushOperator<User>,
     }
   );
-
   if (insertedInfo.modifiedCount === 0) {
     throw new StatusError(500, 'Failed to add notifications to database.');
   }
-
-  for (const follower of followers) {
-    sendNotification(follower._id.toHexString());
-  }
+  // for (const follower of followers) {
+  //   sendNotification(follower._id.toHexString());
+  // }
 };
-
 export async function favoriteArticle(
   userId: string,
   articleId: string
 ): Promise<string[]> {
   const usersCollection = await users();
-
   const user = await usersCollection.findOne({
     _id: ObjectId.createFromHexString(userId),
   });
-
   if (user === null) {
     throw new StatusError(404, 'User not found');
   }
-
   const userData = validateWithType<User>(userSchema, user, 500);
-
   const favoriteArticles = userData.favoriteArticles;
-
   const articleExists = favoriteArticles.findIndex(
-    (value) => value.toString() === articleId
+    (value) => value === articleId
   );
-
   if (articleExists === -1) {
-    favoriteArticles.push(ObjectId.createFromHexString(articleId));
+    favoriteArticles.push(articleId);
   } else {
     throw new StatusError(400, 'Article already favorited');
   }
-
   const insertedInfo = await usersCollection.updateOne(
     {
       _id: ObjectId.createFromHexString(userId),
@@ -92,44 +77,34 @@ export async function favoriteArticle(
       },
     }
   );
-
   if (insertedInfo.modifiedCount === 0) {
     throw new StatusError(500, 'Failed to favorite article');
   }
-
+  console.log("adding articleId");
   addNotifications(userId, articleId);
-
-  return favoriteArticles.map((value) => value.toHexString());
+  return favoriteArticles;
 }
-
 export async function unfavoriteArticle(
   userId: string,
   articleId: string
 ): Promise<string[]> {
   const usersCollection = await users();
-
   const user = await usersCollection.findOne({
     _id: ObjectId.createFromHexString(userId),
   });
-
   if (user === null) {
     throw new StatusError(404, 'User not found');
   }
-
   const userData = validateWithType<User>(userSchema, user, 500);
-
   const favoriteArticles = userData.favoriteArticles;
-
   const articleExists = favoriteArticles.findIndex(
-    (value) => value.toString() === articleId
+    (value) => value === articleId
   );
-
   if (articleExists === -1) {
     throw new StatusError(400, 'Article not favorited');
   } else {
     favoriteArticles.splice(articleExists, 1);
   }
-
   const insertedInfo = await usersCollection.updateOne(
     {
       _id: ObjectId.createFromHexString(userId),
@@ -140,10 +115,8 @@ export async function unfavoriteArticle(
       },
     }
   );
-
   if (insertedInfo.modifiedCount === 0) {
     throw new StatusError(500, 'Failed to unfavorite article');
   }
-
-  return favoriteArticles.map((value) => value.toHexString());
+  return favoriteArticles;
 }
