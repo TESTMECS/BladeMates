d0b8c2e5d3781feb1b45cafccd490cc80fac5c1d
SpicyMcSpice1938 <badmd67@gmail.com>
2024-12-12 21:39:23 -0500

    restored redsing article of the week with ioredis

--- backend/src/routes/articleOfTheWeek.ts
+++ backend/src/routes/articleOfTheWeek.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { handleRouteError } from '../utils/Error';
 import { getArticleOfTheWeek } from '../data/articles';
+import { redisConnection } from '../config/redisConnection';
 declare module 'express-session' {
   interface SessionData {
     userId: string;
@@ -8,6 +9,28 @@ declare module 'express-session' {
 }
 const router = express.Router();
 router.route('/').get(async (req, res) => {
+
+  try {
+    if (req.session.userId === undefined) {
+        res.status(400).send('User not logged in');
+        return;
+    }
+  } catch (error) {
+      handleRouteError(error, res);
+  }
+
+  const client = await redisConnection();
+  let exists = await client?.exists('articleOTW');
+
+  if (exists) {
+      let cachedArticle = await client?.get('articleOTW');
+      if (cachedArticle) {
+          res.status(200).send({ data: JSON.parse(cachedArticle) });
+          return;
+      }
+  }
+
+
   try {
     const article = await getArticleOfTheWeek();
     res.status(200).send({ data: article });
