42aab49ae70209d351029f8e185eb88c05bc3c46
Prithvinarayan Revuri <prevuri@stevens.edu>
2024-12-14 18:29:11 -0500

    Fix: Article expires at end of current week.

--- backend/src/routes/articleOfTheWeek.ts
+++ backend/src/routes/articleOfTheWeek.ts
@@ -23,7 +23,9 @@ router.route('/').get(async (req, res) => {
         }
 
         const article = await getArticleOfTheWeek();
+        //create articleOTW with an expiration date of next week
         await client?.set('articleOTW', JSON.stringify(article));
+        await client?.expireAt('articleOTW', Math.floor(new Date().setDate(new Date().getDate() + 7) / 1000));
         res.status(200).send({ data: article });
     } catch (error) {
         handleRouteError(error, res);
